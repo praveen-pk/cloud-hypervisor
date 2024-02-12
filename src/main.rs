eaddb5238defe2f758219e93709842765515faea
Praveen K Paladugu <prapal@linux.microsoft.com>
2024-02-12 19:18:08 +0000

    vmm: Apply landlock to event-monitor thread
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>

--- src/main.rs
+++ src/main.rs
@@ -633,6 +633,8 @@ fn start_vmm(cmd_arguments: ArgMatches) -> Result<Option<String>, Error> {
     let vm_debug_evt = EventFd::new(EFD_NONBLOCK).map_err(Error::CreateDebugEventFd)?;
 
     let exit_evt = EventFd::new(EFD_NONBLOCK).map_err(Error::CreateExitEventFd)?;
+    // TODO: landlock_enable is being parsed in 2 places: here and VMparams:parse() how to merge both?
+    let landlock_enable = cmd_arguments.get_flag("landlock");
 
     #[allow(unused_mut)]
     let mut event_monitor = cmd_arguments
@@ -702,6 +704,7 @@ fn start_vmm(cmd_arguments: ArgMatches) -> Result<Option<String>, Error> {
         vmm::start_event_monitor_thread(
             monitor,
             &seccomp_action,
+            landlock_enable,
             hypervisor.hypervisor_type(),
             exit_evt.try_clone().unwrap(),
         )
