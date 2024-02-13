6b77c841e2fd55dbf96167b8ac69d30632a5ea7e
Praveen K Paladugu <prapal@linux.microsoft.com>
2024-02-13 18:47:37 +0000

    vmm: Introduce landlock cmdline parameter
    Users can use this cmdline option to enable/disable Landlock LSM while
    starting cloud-hypervisor.
    
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>

--- vmm/src/vm_config.rs
+++ vmm/src/vm_config.rs
@@ -598,4 +598,5 @@ pub struct VmConfig {
     // valid, and will be closed when the holding VmConfig instance is destroyed.
     #[serde(skip)]
     pub preserved_fds: Option<Vec<i32>>,
+    pub landlock_enable: bool,
 }
