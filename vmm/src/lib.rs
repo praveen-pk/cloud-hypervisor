f19c4922bf4105372a3f3b0d3111a35f95058fcf
Praveen K Paladugu <prapal@linux.microsoft.com>
2024-02-15 21:53:11 +0000

    vmm: Enable Landlock on http-server thread
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -484,6 +484,7 @@ pub fn start_vmm_thread(
             seccomp_action,
             exit_event,
             hypervisor_type,
+            landlock_enable,
         )?)
     } else if let Some(http_fd) = http_fd {
         Some(api::start_http_fd_thread(
@@ -493,6 +494,7 @@ pub fn start_vmm_thread(
             seccomp_action,
             exit_event,
             hypervisor_type,
+            landlock_enable,
         )?)
     } else {
         None
